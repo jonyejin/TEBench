e629da5b7d420f3074047b910db57ebb1e96263e
yejin <i3pivigui@icloud.com>
2024-04-30 10:44:23 -0700

    Update submodules to latest commit

--- fetch_submodule.sh
+++ fetch_submodule.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cd ~/TEBench
+current_dir=$(basename "$(pwd)")
+if [ "$current_dir" != "TEBench" ]; then
+    echo "This script must be run from a TEBench directory."
+    exit 1
+fi
+
 git submodule update --remote --merge
 git add .
 git commit -m "Update submodules to latest commit"
